99e4b9394cfebeeabbcc2267c257dd00f2e6992e
JackSutton1231 <2024sutton.jack@fcboe.org>
2025-01-24 13:48:33 -0500

    Create Draw_scene.py

---
+++ Draw_scene.py
@@ -0,0 +1,38 @@
+size(800,800);
+background(0);
+ellipseMode(CENTER);
+// earth
+fill(1,0,255);
+ellipse(220,150,100,100);
+
+// mars
+fill(255,0,1);
+ellipse(700,180,100,100);
+
+// sun
+fill(255,255,0);
+ellipse(405,430,400,400);
+
+// jupiter
+fill(0,235,250);
+ellipse(90,500,100,100);
+
+//black hole
+fill(60,60,60);
+stroke(0);
+ellipse(550,600,300,300);
+
+// SPace SHip
+fill(250,20,100);
+rect(310,60,60,20);
+
+//fire
+stroke(255,255,0);
+line(310,63,280,70);
+line(280,70,310,70);
+line(310,70,280,80);
+line(280,80,310,80);
+
+//pluto
+fill(10,90,200);
+ellipse(500,100,200,200);
